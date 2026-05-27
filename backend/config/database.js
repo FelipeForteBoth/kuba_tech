6af38a38f8029c77f9c087206a42438371ae126d
zanintt <tiago.zanint@gmail.com>
2026-05-26 21:27:14 -0300

    alteração da senha do worckbank

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // Altere para seu usuário
-    password: '12092204070806072023', // Altere para sua senha
+    password: 'admin', // Altere para sua senha
     database: 'kuba_tech',
     waitForConnections: true,
     connectionLimit: 10,
