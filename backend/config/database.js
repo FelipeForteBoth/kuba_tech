09daa37aff783ae7ad4fc5f73793ed264ffc0352
ErickLuan009 <luanerick958@gmail.com>
2026-06-02 21:44:00 -0300

    Database Funcionando

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,16 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
 require('dotenv').config()
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
-connection.connect((err) => {
-    if(err){
-        console.log('Erro banco:', err)
-        return
-    }
+console.log('Banco configurado')
 
-    console.log('Banco conectado')
-})
-
-module.exports = connection
+module.exports = pool
